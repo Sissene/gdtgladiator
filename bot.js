e40cd5da6b12fc35a0a6fb4b460186759b78ab62
Sissene <36569327+Sissene@users.noreply.github.com>
2018-02-18 17:07:49 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,1 +1,16 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
+bot.on('ready', function () {
+    bot.user.setGame('#GDT2RC Soon !').catch(console.error)
+})
+
+
+
+bot.on('message', function (message) {
+    if (message.content === '!tournament') {
+        message.channel.send('The Tournament Will Start Soon !')
+    }
+})
+
+bot.login(process.env.BOT_TOKEN)
