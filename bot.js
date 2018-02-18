a2d5148101cc641a4ae20f420dc86086fe839101
Sissene <36569327+Sissene@users.noreply.github.com>
2018-02-18 20:06:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-bot.on('ready', function () {
-    bot.user.setGame('#GDT2RC Soon !').catch(console.error)
-})
 
-
-
-bot.on('message', (message) => {
-    if(msg.content.startsWith("!tournament")){
-        message.channel.send('The Tournament Will Start Soon !')
-       }
-})
 
 bot.login(process.env.BOT_TOKEN)
