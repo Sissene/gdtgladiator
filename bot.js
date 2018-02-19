9202adfd3cb365aa7809ee3a352eeb8ef865f800
Sissene <36569327+Sissene@users.noreply.github.com>
2018-02-19 15:25:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
 
-  bot.user.setActivity("GDT2RC Soon !", {type: "WATCHING"})
+  bot.user.setActivity("GDT2RC Soon !", {type: "WATCHING"});
   
   bot.on("message", async message => {
  
