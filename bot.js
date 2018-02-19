bd0fbf415461a7759ef2cf10e64fe60ce4c9a2b4
Sissene <36569327+Sissene@users.noreply.github.com>
2018-02-19 15:11:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,23 @@ bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
 
   bot.user.setActivity("GDT2RC Soon !", {type: "WATCHING"})
+  
+  bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfigs.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+    
+    if(cmd === `${prefix}kick`){
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: I can't Kick this user !");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: That person can't be kicked!");
  
 
   
