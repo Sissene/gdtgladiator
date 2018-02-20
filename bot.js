f3c2bd58bea2946bbb5cb501994c05f26678b3c9
Sissene <36569327+Sissene@users.noreply.github.com>
2018-02-20 14:28:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ bot.on("ready", async () => {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You haven't Permissions !");
     if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
     let kickEmbed = new Discord.RichEmbed()
@@ -49,7 +49,7 @@ bot.on("ready", async () => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("This User doesn't Exist !");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("You haven't Permission !");
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Can't kick Person Higher Rank !");
 
     let banEmbed = new Discord.RichEmbed()
