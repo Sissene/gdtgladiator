a4893d4daea7bb923836eeef6896b4d30a06d670
Sissene <36569327+Sissene@users.noreply.github.com>
2018-02-20 14:26:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
 
-  bot.user.setActivity("GDT2RC Soon !", {type: "WATCHING"})});
+  bot.user.setActivity("GDT2RC Soon !", {type: "PLAYING"})});
   
   bot.on("message", async message => {
      if(message.author.bot) return;
@@ -21,10 +21,10 @@ bot.on("ready", async () => {
     //!kick @daeshan askin for it
 
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("This User doesn't Exist !");
+    if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Can't kick Person Higher Rank !");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
     let kickEmbed = new Discord.RichEmbed()
     .setDescription("~Kick~")
@@ -44,7 +44,33 @@ bot.on("ready", async () => {
     return;
   }
       
-    
+  if(cmd === `${prefix}ban`){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("This User doesn't Exist !");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Can't kick Person Higher Rank !");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Ban~")
+    .setColor("#bc0000")
+    .addField("Banned User", `${bUser} ID : ${bUser.id}`)
+    .addField("Banned By", `<@${message.author.id}> ID : ${message.author.id}`)
+    .addField("Banned In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "logs");
+    if(!incidentchannel) return message.channel.send("Can't find logs channel.");
+
+    message.guild.member(bUser).ban(bReason);
+    incidentchannel.send(banEmbed);
+
+
+    return;
+  }
+ 
     
     
     
